c3cae173ec1a05cc46e87c9b076a59f7018e98a7
Autio <petri.mikael.autio@gmail.com>
2021-05-14 00:47:34 +0300

    Hyperlinks in text

--- Assets/Scripts/StoryController.cs
+++ Assets/Scripts/StoryController.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+
+using TMPro;
+
 
 public class StoryController : Singleton<StoryController>
 {
+    [SerializeField]
+    private TextMeshProUGUI EncounterText;
+
+    private string encounter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        encounter = firstPart[Random.Range(0,firstPart.Length)] + " " + secondPart[Random.Range(0,secondPart.Length)];
+        UpdateEncounterText(encounter);
     }
 
     // Update is called once per frame
@@ -16,10 +25,16 @@ public class StoryController : Singleton<StoryController>
         
     }
 
+    public void UpdateEncounterText(string s)
+    {
+        // Maximum length?
+        EncounterText.text = s;
+    }
+
     string[] firstPart = {
-        "Example text 1",
-        "Example text 2", 
-        "Example text 3"
+        "Example text 1 <color=blue><link id=1>my link</color></link>",
+        "Example text 2 <color='blue'>my link</color>", 
+        "Example text 3 <color='blue'>my link</color>"
     };   
     string[] secondPart = {
         "Example text 4",
